41875548c9d8de6f7057a5017172551877db4adc
FullStackRakibul <rakibul.cse.47@gmail.com>
2025-07-07 07:05:52 +0600

    zip remove

---
+++ billingChannel24/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import HeaderComponent from "@/layouts/HeaderComponent.vue";
+</script>
+
+<template>
+  <HeaderComponent />
+  <RouterView />
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {}
+</style>
